a468149512482416d031b35211a1aae0bb25ee3d
Gaurav Rawat <yashu.rawat1903@gmail.com>
2024-06-07 06:04:53 +0530

    chnage ui

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TextField from './TextField';
 function App() {
   return (
     <div  className="App">
+      <h1>HI everyone</h1>
       <TextField heading={"Enter your prompt "}  bt1={"to upper "}  bt2={"to lower"}  bt3={"to reverse"}/>    
     </div>
   );
